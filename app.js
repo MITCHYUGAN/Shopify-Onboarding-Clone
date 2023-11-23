dcfc03f3b6fd2b12bd0535191243e1d114853e3c
MITCHYUGAN <denmichigan123@gmail.com>
2023-11-23 13:51:05 +0100

    Finished Header styling and functionality and added html contents for the onboarding process first div

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const closeSelectplanBtn = document.querySelector(".closeselectplan")
 const selectplan = document.querySelector(".selectplan")
 const notification = document.querySelector(".notification")
 const notificationcontents = document.querySelector(".notificationcontents")
+const profile = document.querySelector(".profile")
+const profilecontents = document.querySelector(".profilecontents")
+
 
 // Close Select Plan
 closeSelectplanBtn.addEventListener("click", () => {
@@ -11,4 +14,9 @@ closeSelectplanBtn.addEventListener("click", () => {
 // Toggle Notification
 notification.addEventListener("click", () => {
     notificationcontents.classList.toggle("active")
+})
+
+//Toggle Profile
+profile.addEventListener("click", () => {
+    profilecontents.classList.toggle("active")
 })
