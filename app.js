af6a39ef09dd6ced905d89d75e6ad365d2e01d46
MITCHYUGAN <denmichigan123@gmail.com>
2023-11-23 10:51:27 +0100

    Finished the Notification styling and functionality

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const closeSelectplanBtn = document.querySelector(".closeselectplan")
 const selectplan = document.querySelector(".selectplan")
+const notification = document.querySelector(".notification")
+const alerts = document.querySelector(".alerts")
 
+// Close Select Plan
 closeSelectplanBtn.addEventListener("click", () => {
     selectplan.remove()
+})
+
+// Toggle Notification
+notification.addEventListener("click", () => {
+    alerts.classList.toggle("active")
 })
